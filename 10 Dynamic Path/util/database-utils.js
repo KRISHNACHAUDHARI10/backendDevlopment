016bcab9e6ae3769453b894f52559db2297f321c
Krishna <Krishnachaudhari0340@gmail.com>
2025-09-08 20:16:10 +0530

    Initial backendDevlopment upload

---
+++ 10 Dynamic Path/util/database-utils.js
@@ -0,0 +1,9 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const url  ="mongodb+srv://:<db_password>@cluster0.jodalfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const mongoConnect =(callback)=>{
+
+}
